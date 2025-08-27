88e079632a662be7515fad696625843a2ca4385f
mohamad <mo.mohamadashrafi1@gmail.com>
2025-08-28 01:45:46 +0330

    chore(FieldSettings.vue): change filed setting.vue

---
+++ src/components/FieldSettings.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h5 class="mb-3 mt-4">تنظیمات فیلد</h5>
+    <div class="card">
+      <div class="card-body">
+        <p class="text-muted small text-center">فیلدی را انتخاب کنید</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldSettings'
+}
+</script>
