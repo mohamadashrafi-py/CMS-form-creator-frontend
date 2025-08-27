851bc2dfc823f8d554cd9b55f3fb16bef70c4ae0
mohamad <mo.mohamadashrafi1@gmail.com>
2025-08-28 01:40:50 +0330

    chore(LivePreview.vue): add live preview component

---
+++ src/components/LivePreview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h5 class="mb-3">پیش نمایش زنده</h5>
+    <div class="card mb-3">
+      <div class="card-body p-2">
+        <div class="p-2">
+          <p class="text-muted small text-center">پیش نمایش اینجا نمایش داده می‌شود</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LivePreview'
+}
+</script>
